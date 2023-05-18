2eeef0f3b67153c6ccece615f6c13839637d9058
descrysama <erwing2002@gmail.com>
2023-05-18 17:28:29 +0200

    script

--- controllers/scriptController.js
+++ controllers/scriptController.js
@@ -1,22 +1,20 @@
 const { exec } = require('child_process');
 
 async function runScript(req, res) {
-    return new Promise((resolve, reject) => {
-        exec('bash your_script.sh', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Error: ${error.message}`);
-                reject(error);
-                return;
-            }
-            if (stderr) {
-                console.error(`Script error: ${stderr}`);
-                reject(new Error(stderr));
-                return;
-            }
-            console.log(`Script output: ${stdout}`);
-            resolve();
-        });
-    });
+    exec('bash your_script.sh', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error: ${error.message}`);
+          res.status(500).send('An error occurred while executing the script.');
+          return;
+        }
+        if (stderr) {
+          console.error(`Script error: ${stderr}`);
+          res.status(500).send('An error occurred in the script.');
+          return;
+        }
+        console.log(`Script output: ${stdout}`);
+        res.send('Script execution completed successfully.');
+      });
 }
 
 module.exports = {
