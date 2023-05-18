6f9a6cf4e4c56e3721914666d4dd6f53f6c066a5
descrysama <erwing2002@gmail.com>
2023-05-18 18:29:48 +0200

    update messager

--- controllers/scriptController.js
+++ controllers/scriptController.js
@@ -4,16 +4,16 @@ async function runScript(req, res) {
     exec('bash /var/www/scripts/main_runner.sh', (error, stdout, stderr) => {
         if (error) {
           console.error(`Error: ${error.message}`);
-          res.status(500).send('An error occurred while executing the script.');
+          res.status(500).json({error: 'An error occurred while executing the script.'});
           return;
         }
         if (stderr) {
           console.error(`Script error: ${stderr}`);
-          res.status(500).send('An error occurred in the script.');
+          res.status(500).json({error: 'An error occurred in the script.'});
           return;
         }
         console.log(`Script output: ${stdout}`);
-        res.send('Script execution completed successfully.');
+        res.status(200).json({message: 'Exécution du terminée. Retrouvez le sur http://79.137.87.52/final_output.xlsx'});
       });
 }
 
