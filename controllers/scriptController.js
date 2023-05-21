38a5e34f26a81929b9e2880fc797f80a1bc0f67b
descrysama <erwing2002@gmail.com>
2023-05-21 10:47:31 +0200

    update back

--- controllers/scriptController.js
+++ controllers/scriptController.js
@@ -17,6 +17,24 @@ async function runScript(req, res) {
       });
 }
 
+async function runMain(req, res) {
+  exec('bash /var/www/scripts/competitor_scripts/runfinalscript.sh', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      res.status(500).json({error: 'An error occurred while executing the script.'});
+      return;
+    }
+    if (stderr) {
+      console.error(`Script error: ${stderr}`);
+      res.status(500).json({error: 'An error occurred in the script.'});
+      return;
+    }
+    console.log(`Script output: ${stdout}`);
+    res.status(200).json({message: 'Exécution du terminée. Retrouvez le sur http://79.137.87.52/final_output.xlsx'});
+  });
+}
+
 module.exports = {
-    runScript
+    runScript,
+    runMain
 };
--- routes/scriptRoutes.js
+++ routes/scriptRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const scriptController = require('../controllers/scriptController');
 
 router.get('/get', scriptController.runScript);
+router.get('/runmain', scriptController.runMain);
 
 module.exports = router;
